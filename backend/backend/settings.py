42b5e022fe6e4a6f1cbda30452946d3d5ba4c6dd
Christa Boban <christaboban.official@gmail.com>
2026-08-17 11:00:47 +0530

    Connect Django to PostgreSQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,6 +15,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +82,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
